37f36ecba2bab8700fc068142575d15b254c2876
Kate Lynn Thomson <152451043+klynn808@users.noreply.github.com>
2024-10-22 11:23:53 -0500

    Add files via upload

---
+++ FizzBuzzChallenge/FizzBuzzChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzzChallenge
+{
+    class Program
+    {
+        public string FizzWord(int number)
+        {
+            bool divisibleBy3 = number % 3 == 0;
+            bool divisibleBy5 = number % 5 == 0;
+
+            return (divisibleBy3 && divisibleBy5) ? "fizzbuzz" :
+                (divisibleBy3) ? "fizz" :
+                (divisibleBy5) ? "buzz" : number.ToString();
+        }
+    }
+}
